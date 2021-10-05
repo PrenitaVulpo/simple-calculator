ec38fcacb3acb55726b7f72aef2e4ec1e196de02
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-10-05 18:10:41 -0300

    add clear function

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
+    this.clear()
   }
 
-  clear() { }
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = ''
+  }
 
   delete() { }
 
