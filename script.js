b9fe71b3fe332cf64ff3172425be371b93464292
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-10-05 18:14:31 -0300

    add event listener to each button

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ class Calculator {
   clear() {
     this.currentOperand = ''
     this.previousOperand = ''
-    this.operation = ''
+    this.operation = undefined
   }
 
   delete() { }
@@ -30,3 +30,12 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
